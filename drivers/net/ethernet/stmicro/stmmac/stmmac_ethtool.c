68a5ac437a47472b5e9f37031d9f854121878d03
jinmin.xu <jinmin.xu@advantech.com.cn>
2020-07-10 13:07:16 +0800

    mask deepin net wol warning

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -567,6 +567,11 @@ static int stmmac_set_wol(struct net_device *dev, struct ethtool_wolinfo *wol)
 	struct stmmac_priv *priv = netdev_priv(dev);
 	u32 support = WAKE_MAGIC | WAKE_UCAST;
 
+	#ifdef CONFIG_ARCH_ADVANTECH
+	if(wol->wolopts == 0)
+		return -EINVAL;
+	#endif
+
 	/* By default almost all GMAC devices support the WoL via
 	 * magic frame but we can disable it if the HW capability
 	 * register shows no support for pmt_magic_frame. */
