12f50ea69b6e875308b0bdb9c3e530d20398cf39
huxt <635499244@qq.com>
2022-09-07 15:15:56 +0800

    for emmc ext_csd

--- include/linux/mmc/mmc.h
+++ include/linux/mmc/mmc.h
@@ -266,6 +266,9 @@ static inline bool mmc_ready_for_data(u32 status)
 #define EXT_CSD_BKOPS_START		164	/* W */
 #define EXT_CSD_SANITIZE_START		165     /* W */
 #define EXT_CSD_WR_REL_PARAM		166	/* RO */
+#ifdef CONFIG_ARCH_ADVANTECH
+#define EXT_CSD_WRITE_RELIABLIITY	167	/* R/W */
+#endif
 #define EXT_CSD_RPMB_MULT		168	/* RO */
 #define EXT_CSD_FW_CONFIG		169	/* R/W */
 #define EXT_CSD_BOOT_WP			173	/* R/W */
